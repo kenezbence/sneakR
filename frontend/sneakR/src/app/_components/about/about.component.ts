714d6f0b5147cf0cf28485f7454a6d282d85334f
kreszltms <90987072+kreszltms@users.noreply.github.com>
2025-02-03 11:05:07 +0100

    InfoPage-redesign-update
    InfoPage-redesign-update

--- frontend/sneakR/src/app/_components/about/about.component.ts
+++ frontend/sneakR/src/app/_components/about/about.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  cartCount = 2; 
+  isMenuActive = false;
 
+  constructor(private router: Router) {}
+
+  navigateToTarget3() {
+    this.router.navigate(['/select']);
+  }
+
+  toggleMenu() {
+    this.isMenuActive = !this.isMenuActive;
+  }
 }
--- frontend/sneakR/src/app/_components/gyik/gyik.component.ts
+++ frontend/sneakR/src/app/_components/gyik/gyik.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-gyik',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, CommonModule,RouterModule],
   templateUrl: './gyik.component.html',
   styleUrl: './gyik.component.css'
 })
 export class GYIKComponent {
+  cartCount = 2; 
+  isMenuActive = false;
 
+  constructor(private router: Router) {}
+
+  navigateToTarget3() {
+    this.router.navigate(['/select']);
+  }
+
+  toggleMenu() {
+    this.isMenuActive = !this.isMenuActive;
+  }
 }
