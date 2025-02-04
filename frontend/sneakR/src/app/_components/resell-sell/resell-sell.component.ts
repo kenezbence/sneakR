90c51f2122497e2147d4a35e2b6e03204d7c2b89
kreszltms <90987072+kreszltms@users.noreply.github.com>
2025-02-04 19:01:17 +0100

    Update resell-sell
    Update resell-sell

--- frontend/sneakR/src/app/_components/resell-sell/resell-sell.component.ts
+++ frontend/sneakR/src/app/_components/resell-sell/resell-sell.component.ts
@@ -1,33 +1,53 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resell-sell',
   standalone: true,
-  imports: [NavbarComponent, FormsModule],
+  imports: [NavbarComponent, FormsModule, CommonModule],
   templateUrl: './resell-sell.component.html',
   styleUrls: ['./resell-sell.component.css']
 })
 export class ResellSellComponent {
   constructor(private router: Router) {}
 
+  showSuccessModal: boolean = false;
+  selectedFiles: File[] = [];
+  imageError: boolean = false;
+  showValidationError: boolean = false;
+  missingFields: string[] = [];
+  
   onFileSelect(event: Event): void {
     const input = event.target as HTMLInputElement;
-    if (input?.files?.length) {
-      console.log(`${input.files.length} kép kiválasztva.`);
+    if (input.files?.length) {
+      this.selectedFiles = Array.from(input.files);
+      this.imageError = false;
+    } else {
+      this.selectedFiles = [];
     }
   }
 
   onCancel(): void {
-    // Mégse gomb navigáció az előző oldalra
     this.router.navigate(['/resell']);
   }
 
-  onSubmit(event: Event): void {
-    event.preventDefault();
-    alert('Sikeres feltöltés!');
-    // Itt jöhet az API hívás vagy további logika
+  onSubmit(form: NgForm): void {
+    if (form.invalid || this.selectedFiles.length === 0) {
+      form.control.markAllAsTouched();
+      this.imageError = this.selectedFiles.length === 0;
+      return;
+    }
+    
+    this.showSuccessModal = true;
+  }
+
+
+
+  onCloseSuccessModal(): void {
+    this.showSuccessModal = false;
+    this.router.navigate(['/resell']);
   }
 }
