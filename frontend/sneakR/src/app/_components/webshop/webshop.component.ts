b787d956b84b8ea9366613a69b18f0fb92dbc9b9
kenezbence <bencekenez1@gmail.com>
2025-02-04 09:53:42 +0100

    webshop_hamburgermenu_update

--- frontend/sneakR/src/app/_components/webshop/webshop.component.ts
+++ frontend/sneakR/src/app/_components/webshop/webshop.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
-@Component({
-  selector: 'app-webshop',
-  standalone: true,
-  imports: [],
-  templateUrl: './webshop.component.html',
-  styleUrl: './webshop.component.css'
-})
-export class WebshopComponent {
-  isMenuOpen = false;
-
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-    
-    // Scroll lock, ha a menü nyitva van
-    if (this.isMenuOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
+  @Component({
+    selector: 'app-webshop',
+    standalone: true,
+    imports: [RouterLink],
+    templateUrl: './webshop.component.html',
+    styleUrl: './webshop.component.css'
+  })
+  export class WebshopComponent {
+    isMenuOpen = false;
+    menuState: { [key: string]: boolean } = {};
+  
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+      if (this.isMenuOpen) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'auto';
+        this.menuState = {}; // Reset submenü állapotok
+      }
+    }
+  
+    toggleSubmenu(menuKey: string) {
+      this.menuState[menuKey] = !this.menuState[menuKey];
     }
   }
-}
