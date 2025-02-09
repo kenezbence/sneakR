f85ee339b9cf4a2d6295dd7ab03ee69b3d31c442
kreszltms <90987072+kreszltms@users.noreply.github.com>
2025-02-09 14:33:52 +0100

    Upload everything that new
    Upload everything that new

--- frontend/sneakR/src/app/_components/resell/resell.component.ts
+++ frontend/sneakR/src/app/_components/resell/resell.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-resell',
   standalone: true,
-  imports: [NavbarComponent,RouterLink, RouterOutlet],
+  imports: [NavbarComponent,RouterLink],
   templateUrl: './resell.component.html',
   styleUrl: './resell.component.css'
 })
