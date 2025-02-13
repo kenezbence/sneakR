bcb6d0b714b83e25e9ccb28f3c13b0505645dbfb
KriszToth <2004kiki01@gmail.com>
2025-02-13 11:44:38 +0100

    resellShoeUpload

--- backend/sneakRproject/src/main/java/sneakr/sneakrproject/controller/ResellShoeController.java
+++ backend/sneakRproject/src/main/java/sneakr/sneakrproject/controller/ResellShoeController.java
@@ -5,7 +5,9 @@
 package sneakr.sneakrproject.controller;
 
 import java.util.ArrayList;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -13,6 +15,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import sneakr.sneakrproject.model.ResellCipok;
 import sneakr.sneakrproject.service.ResellShoeService;
@@ -80,7 +83,36 @@ public Response getAllResellShoesData() {
 }  
      
      
-     
+@POST
+@Path("uploadResellShoes")
+@Consumes(MediaType.APPLICATION_JSON)
+public Response uploadResellShoes(String bodyString) {
+    try {
+        JSONObject body = new JSONObject(bodyString);
+        
+        ResellCipok u = new ResellCipok(
+            body.getString("nev"),
+            body.getString("marka"),
+            body.getString("nem"), 
+            body.getString("allapot"),
+            body.getInt("meret"),
+            (float) body.getDouble("ar"),
+            body.optString("img", ""),
+            body.getInt("userId")
+        );
+        
+        JSONObject obj = layer.uploadResellShoes(u);
+        return Response.status(obj.getInt("statusCode"))
+                       .entity(obj.toString())
+                       .build();
+    } catch (JSONException e) {
+        JSONObject error = new JSONObject();
+        error.put("status", "InvalidRequest");
+        error.put("statusCode", 400);
+        error.put("message", "Malformed JSON input");
+        return Response.status(400).entity(error.toString()).build();
+    }
+}
      
      
      
