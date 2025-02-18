25f6357cda64d31f70af764d216d489106444c94
kreszltms <90987072+kreszltms@users.noreply.github.com>
2025-02-18 13:25:03 +0100

    Create Success Modal

--- frontend/sneakR/src/app/_components/resell-cart/resell-cart.component.ts
+++ frontend/sneakR/src/app/_components/resell-cart/resell-cart.component.ts
@@ -3,10 +3,15 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { ResellCartService, CartProduct } from '../../_services/resell-cart.service';
 
+
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-resell-cart',
   standalone: true,
-  imports: [NavbarComponent, CommonModule],
+  imports: [NavbarComponent, CommonModule, FormsModule],
   templateUrl: './resell-cart.component.html',
   styleUrls: ['./resell-cart.component.css']
 })
@@ -14,8 +19,13 @@ export class ResellCartComponent implements OnInit {
   cart: CartProduct[] = [];
   showCard = false;
   isMenuActive = false;
+  showSuccessModal = false;
 
-  constructor(private cartService: ResellCartService) {}
+
+  constructor(private cartService: ResellCartService,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
     this.cartService.getCart().subscribe(cart => {
@@ -31,15 +41,19 @@ export class ResellCartComponent implements OnInit {
     this.showCard = paymentMethod === 'bankkártya';
   }
 
-  submitCheckout(event: Event, name: string, email: string, city: string, zip: string, street: string, number: string): void {
-    event.preventDefault();
-    const address = `${street} ${number}, ${city} ${zip}`;
-    alert(`Kosarad elküldve!\nNév: ${name}\nEmail: ${email}\nCím: ${address}`);
-    this.cartService.clearCart();
-}
-
   getTotal(): number {
     return this.cart.reduce((acc, item) => acc + item.price, 0);
 }
 
+  submitCheckout(event: Event, name: string, email: string, city: string, zip: string, street: string, number: string): void {
+    event.preventDefault();
+    const address = `${street} ${number}, ${city} ${zip}`;
+    this.cartService.clearCart();
+    this.showSuccessModal = true;
+  }
+
+  onCloseSuccessModal(): void {
+    this.showSuccessModal = false;
+    this.router.navigate(['/resell']); // Or your resell route
+}
 }
