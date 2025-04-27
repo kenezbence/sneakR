4de09cd6cfb675fa8d76312b2e59f7a033759fa1
KriszToth <2004kiki01@gmail.com>
2025-04-27 12:53:56 +0200

    test_create

---
+++ backend/sneakRproject/src/test/java/sneakr/sneakrproject/model/UserekTest.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package sneakr.sneakrproject.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author TothKrisztian
+ */
+public class UserekTest {
+    
+    public UserekTest() {
+    }
+
+    @org.junit.Test
+    public void testGetId() {
+    }
+
+    @org.junit.Test
+    public void testSetId() {
+    }
+
+    @org.junit.Test
+    public void testGetNev() {
+    }
+
+    @org.junit.Test
+    public void testSetNev() {
+    }
+
+    @org.junit.Test
+    public void testGetEmail() {
+    }
+
+    @org.junit.Test
+    public void testSetEmail() {
+    }
+
+    @org.junit.Test
+    public void testGetJelszo() {
+    }
+
+    @org.junit.Test
+    public void testSetJelszo() {
+    }
+
+    @org.junit.Test
+    public void testGetAdmin() {
+    }
+
+    @org.junit.Test
+    public void testSetAdmin() {
+    }
+
+    @org.junit.Test
+    public void testGetLakcimekCollection() {
+    }
+
+    @org.junit.Test
+    public void testSetLakcimekCollection() {
+    }
+
+    @org.junit.Test
+    public void testGetRendelesTetelekCollection() {
+    }
+
+    @org.junit.Test
+    public void testSetRendelesTetelekCollection() {
+    }
+
+    @org.junit.Test
+    public void testGetRendelesekCollection() {
+    }
+
+    @org.junit.Test
+    public void testSetRendelesekCollection() {
+    }
+
+    @org.junit.Test
+    public void testGetShoppingSessionCollection() {
+    }
+
+    @org.junit.Test
+    public void testSetShoppingSessionCollection() {
+    }
+
+    @org.junit.Test
+    public void testHashCode() {
+    }
+
+    @org.junit.Test
+    public void testEquals() {
+    }
+
+    @org.junit.Test
+    public void testToString() {
+    }
+
+    @org.junit.Test
+    public void testLogin() {
+    }
+
+    @org.junit.Test
+    public void testRegisterUser() {
+    }
+
+    @org.junit.Test
+    public void testIsUserExists() {
+    }
+
+    @org.junit.Test
+    public void testDeleteUser() {
+    }
+
+    @org.junit.Test
+    public void testGetAllUsers() {
+    }
+    
+}
